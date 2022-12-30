e4e8e5dd43dc7ed04227291660a4220678707efb
Kiril <kaloyanov.kiril@gmail.com>
2022-12-30 07:35:06 +0200

    Added menu

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,3 +1,7 @@
 const navBar = document.querySelector('nav');
 const logoDiv = document.createElement('div');
+const socialMediaActions = document.createElement('div');
+const menu = document.createElement('div');
 navBar.appendChild(logoDiv);
+navBar.appendChild(socialMediaActions);
+navBar.appendChild(menu);
