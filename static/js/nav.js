a6fe50cc75fcd9387bcb53f94e1d49acf2383cec
Kiril <kaloyanov.kiril@gmail.com>
2022-12-30 07:41:02 +0200

    Added styles to navbar

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,7 +1,13 @@
 const navBar = document.querySelector('nav');
+
 const logoDiv = document.createElement('div');
+logoDiv.classList = 'logo-div';
+logoDiv.textContent = 'logo';
 const socialMediaActions = document.createElement('div');
+socialMediaActions.classList = 'social-media-actions';
 const menu = document.createElement('div');
+menu.classList = 'menu';
+
 navBar.appendChild(logoDiv);
 navBar.appendChild(socialMediaActions);
 navBar.appendChild(menu);
